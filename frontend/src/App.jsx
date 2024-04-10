4e737acb4e568eb951d07ac103766906a476fadc
Utsav <utsavkasvala123@gmail.com>
2024-04-11 03:17:24 +0530

    Adding tailwing resuable classes and working on navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-
+import Layout from './layout/Layout'
 function App() {
 
   return (
-    <>
-      <h1 className='text-blue-500 text-[40px]'>Hi BRO</h1>
-    </>
+
+    <Layout/> 
+    
   )
 }
 
