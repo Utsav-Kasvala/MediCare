619884f3d0677068f8dfa5700fdde1229bb7b7ab
Utsav-Kasvala <utsavkasvala52@gmail.com>
2024-04-13 13:34:30 +0530

    Working on FeedBack

--- frontend/src/pages/Doctors/DoctorAbout.jsx
+++ frontend/src/pages/Doctors/DoctorAbout.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-
+import { formateDate } from '../../utils/formateDate'
 const DoctorAbout = () => {
   return (
     <div>
@@ -20,20 +20,44 @@ const DoctorAbout = () => {
             <ul className='pt-4 md:p-5'>
                 <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
                     <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>23,June 2004</span>
+                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formateDate("11-12-13")}</span>
                         <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
                     </div>
                     <p className='text-[14px] leading-5 font-medium text-textColor'>New Apollo Hospital, Surat</p>
                 </li>
                 <li className='flex flex-col sm:flex-row sm:justify-between sm:items-end md:gap-5 mb-[30px]'>
                     <div>
-                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>23,June 2004</span>
+                        <span className='text-irisBlueColor text-[15px] leading-6 font-semibold'>{formateDate("11-12-13")}</span>
                         <p className='text-[16px] leading-6 font-medium text-textColor'>PHD in Surgeon</p>
                     </div>
                     <p className='text-[14px] leading-5 font-medium text-textColor'>New Apollo Hospital, Surat</p>
                 </li>
             </ul>
         </div>
+         
+         <div className='mt-12'>
+         <h3 className='text-[20px] leading-[30px] text-headingColor font-semibold'>Experience</h3>
+
+         <ul className='grid sm:grid-cols-2 gap-[30px] pt-4 md:p-5'>
+            <li className='p-4 rounded bg-[#fff9ea]'>
+                <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+                    {formateDate("11-12-13")}
+                </span>
+                <p className='text-[15px] leading-6 font-medium text-textColor'>Sr. Surgeon</p>
+                <p className='text-[15px] leading-6 font-medium text-textColor'>hospital Name</p> 
+
+            </li>
+            <li className='p-4 rounded bg-[#fff9ea]'>
+                <span className='text-yellowColor text-[15px] leading-6 font-semibold'>
+                    {formateDate("11-12-13")}
+                </span>
+                <p className='text-[15px] leading-6 font-medium text-textColor'>Sr. Surgeon</p>
+                <p className='text-[15px] leading-6 font-medium text-textColor'>hospital Name</p> 
+
+            </li>
+         </ul>
+
+         </div>
 
     </div>
   )
