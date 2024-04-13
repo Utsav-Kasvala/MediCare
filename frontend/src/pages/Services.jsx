9c29a401d6cb404cce43a337368b44c8ab0d04f9
Utsav Kasvala <132893881+Utsav-Kasvala@users.noreply.github.com>
2024-04-13 18:09:35 +0530

    Revert "Merging both Branches"

--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,15 +1,8 @@
-import { services } from '../assets/data/services'
-import ServiceCard from '../components/Services/ServiceCard'
+import React from 'react'
 
 const Services = () => {
   return (
-     <section>
-      <div className="container">
-      <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 lg:gap-[30px]'>
-        {services.map((item,index)=> <ServiceCard item={item} index={index} key={index}/>)} 
-       </div>
-      </div>
-     </section>
+    <div>Services</div>
   )
 }
 
